7a18551e7398d9d32c8145f92b92dfff83f66655
lemon <borbtrash@gmail.com>
2026-06-09 12:38:36 -0400

    relocation demo

---
+++ tools/rrasm/relocator.asm
@@ -0,0 +1,28 @@
+org 0xfc00
+
+RELOCATION_ADDRESS equ 0x0010
+
+mov r0, payload
+mov r1, payload_end
+mov r3, RELOCATION_ADDRESS
+cpyloop:
+mov r2, [r0]
+mov [r3], r2
+add r0, 1
+add r3, 1
+blt r1, r0, cpyloop
+
+jmpabs RELOCATION_ADDRESS
+
+payload:
+xor r0, r0
+xor r1, r1
+xor r3, r3
+addi r1, r0, 0x0000
+addi r2, r0, 0x0001
+loop:
+add r3, r1, r2
+add r1, r2, r0
+add r2, r3, r0
+jmp loop
+payload_end:
