60d7f80452252ccf127e8a0014f8abfdc94ce093
Raphael Treccani-Chinelli <raphael.treccanichinelli@gmail.com>
2026-06-15 21:42:21 +0100

    Add vga text glyphs

--- verification.asm
+++ verification.asm
@@ -259,6 +259,36 @@ test_32:
 
 test_33:
     mov r15, 0x0021
+
+test_34:
+    mov r15, 0x0022
+
+    mov r0, 0x1A00
+    mov sp, r0
+
+    mov r12, 0x1A80          ; current row base (8 pixels = 40 words per text row)
+
+    mov r4, str_hello
+    mov r5, r12
+    call draw_string
+    add r12, r12, 320
+
+    mov r4, str_fox1
+    mov r5, r12
+    call draw_string
+    add r12, r12, 320
+
+    mov r4, str_fox2
+    mov r5, r12
+    call draw_string
+    add r12, r12, 320
+
+    add r12, r12, 320        ; blank row
+
+    mov r5, r12
+    call draw_glyph_dump
+
+    mov r15, 0x0023
     halt
 
 
@@ -269,9 +299,950 @@ fail:
     halt
 
 
-test_addnumbers: ; clobbers r1 ? 
+test_addnumbers: ; clobbers r1 ?
     mov r0, sp
     mov r1, [r0 + 1]
     mov r2, [r0 + 2]
     add r1, r2
     ret
+
+
+; draw_string: render a null-terminated ASCII string to the framebuffer
+; r4 = ptr to string (one ASCII char per word, 0 = null terminator)
+; r5 = framebuffer word address of top-left of first character
+; clobbers r0, r1, r3, r4, r5, r6, r7, r8, r9
+draw_string:
+    xor r6, r6, r6
+    mov r7, ascii_glyphs
+draw_string_next:
+    mov r0, [r4]
+    beq r0, r6, draw_string_ret
+    sub r0, r0, 0x20        ; offset from space (first glyph)
+    shl r0, 3               ; * 8 words per glyph
+    add r0, r0, r7          ; glyph address
+    mov r1, r5
+    call draw_glyph
+    add r4, r4, 1
+    add r5, r5, 1
+    jmp draw_string_next
+draw_string_ret:
+    ret
+
+
+; draw_glyph: blit one 8x8 pre-expanded glyph into the framebuffer
+; r0 = font glyph ptr (8 consecutive words of 4bpp pixel data), exits advanced by 8
+; r1 = framebuffer word address of top-left pixel of character cell
+; clobbers r0, r1, r3, r6, r9 (r2 intentionally preserved for caller)
+draw_glyph:
+    xor r6, r6, r6
+    mov r9, 8
+draw_glyph_row:
+    mov r3, [r0]
+    mov [r1], r3
+    add r0, r0, 1
+    add r1, r1, 40
+    sub r9, r9, 1
+    bneq r9, r6, draw_glyph_row
+    ret
+
+
+; draw_glyph_dump: blit every printable ASCII char (0x20-0x7E) starting at r5,
+; wrapping to the next screen row every 40 characters
+; r5 = framebuffer word address of top-left of first cell
+; clobbers r0, r1, r3, r5, r6, r7, r9, r10, r11, r13
+draw_glyph_dump:
+    xor r6, r6, r6
+    mov r7, ascii_glyphs
+    mov r10, 0x20            ; current ASCII code
+    mov r11, 0               ; column counter
+    mov r13, 0x7F            ; one past last glyph
+draw_glyph_dump_next:
+    beq r10, r13, draw_glyph_dump_done
+    mov r0, r10
+    sub r0, r0, 0x20
+    shl r0, 3
+    add r0, r0, r7
+    mov r1, r5
+    call draw_glyph
+    add r5, r5, 1
+    add r10, r10, 1
+    add r11, r11, 1
+    sub r3, r11, 40
+    bneq r3, r6, draw_glyph_dump_next
+    mov r11, 0
+    sub r5, r5, 40
+    add r5, r5, 320
+    jmp draw_glyph_dump_next
+draw_glyph_dump_done:
+    ret
+
+
+; strings for test_34, one ASCII char per word, null-terminated
+str_hello:
+    dd "Hello world!", 0
+
+str_fox1:
+    dd "The quick brown fox", 0
+
+str_fox2:
+    dd "jumped over the lazy dog", 0
+
+
+; ASCII 0x20-0x7E glyphs, pre-expanded to 4bpp, 8 words each
+; pixel 0 (leftmost) in bits [3:0]; index = (ascii - 0x20)
+ascii_glyphs:
+glyph_20:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+glyph_21:
+    dd 0x000FF000
+    dd 0x00FFFF00
+    dd 0x00FFFF00
+    dd 0x000FF000
+    dd 0x000FF000
+    dd 0x00000000
+    dd 0x000FF000
+    dd 0x00000000
+glyph_22:
+    dd 0x0FF0FF00
+    dd 0x0FF0FF00
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+glyph_23:
+    dd 0x0FF0FF00
+    dd 0x0FF0FF00
+    dd 0xFFFFFFF0
+    dd 0x0FF0FF00
+    dd 0xFFFFFFF0
+    dd 0x0FF0FF00
+    dd 0x0FF0FF00
+    dd 0x00000000
+glyph_24:
+    dd 0x00FF0000
+    dd 0x0FFFFF00
+    dd 0xFF000000
+    dd 0x0FFFF000
+    dd 0x0000FF00
+    dd 0xFFFFF000
+    dd 0x00FF0000
+    dd 0x00000000
+glyph_25:
+    dd 0x00000000
+    dd 0xFF000FF0
+    dd 0xFF00FF00
+    dd 0x000FF000
+    dd 0x00FF0000
+    dd 0x0FF00FF0
+    dd 0xFF000FF0
+    dd 0x00000000
+glyph_26:
+    dd 0x00FFF000
+    dd 0x0FF0FF00
+    dd 0x00FFF000
+    dd 0x0FFF0FF0
+    dd 0xFF0FFF00
+    dd 0xFF00FF00
+    dd 0x0FFF0FF0
+    dd 0x00000000
+glyph_27:
+    dd 0x0FF00000
+    dd 0x0FF00000
+    dd 0xFF000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+glyph_28:
+    dd 0x000FF000
+    dd 0x00FF0000
+    dd 0x0FF00000
+    dd 0x0FF00000
+    dd 0x0FF00000
+    dd 0x00FF0000
+    dd 0x000FF000
+    dd 0x00000000
+glyph_29:
+    dd 0x0FF00000
+    dd 0x00FF0000
+    dd 0x000FF000
+    dd 0x000FF000
+    dd 0x000FF000
+    dd 0x00FF0000
+    dd 0x0FF00000
+    dd 0x00000000
+glyph_2A:
+    dd 0x00000000
+    dd 0x0FF00FF0
+    dd 0x00FFFF00
+    dd 0xFFFFFFFF
+    dd 0x00FFFF00
+    dd 0x0FF00FF0
+    dd 0x00000000
+    dd 0x00000000
+glyph_2B:
+    dd 0x00000000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0xFFFFFF00
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00000000
+    dd 0x00000000
+glyph_2C:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x0FF00000
+glyph_2D:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0xFFFFFF00
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+glyph_2E:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00000000
+glyph_2F:
+    dd 0x00000FF0
+    dd 0x0000FF00
+    dd 0x000FF000
+    dd 0x00FF0000
+    dd 0x0FF00000
+    dd 0xFF000000
+    dd 0xF0000000
+    dd 0x00000000
+glyph_30:
+    dd 0x0FFFFF00
+    dd 0xFF000FF0
+    dd 0xFF00FFF0
+    dd 0xFF0FFFF0
+    dd 0xFFFF0FF0
+    dd 0xFFF00FF0
+    dd 0x0FFFFF00
+    dd 0x00000000
+glyph_31:
+    dd 0x00FF0000
+    dd 0x0FFF0000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0xFFFFFF00
+    dd 0x00000000
+glyph_32:
+    dd 0x0FFFF000
+    dd 0xFF00FF00
+    dd 0x0000FF00
+    dd 0x00FFF000
+    dd 0x0FF00000
+    dd 0xFF00FF00
+    dd 0xFFFFFF00
+    dd 0x00000000
+glyph_33:
+    dd 0x0FFFF000
+    dd 0xFF00FF00
+    dd 0x0000FF00
+    dd 0x00FFF000
+    dd 0x0000FF00
+    dd 0xFF00FF00
+    dd 0x0FFFF000
+    dd 0x00000000
+glyph_34:
+    dd 0x000FFF00
+    dd 0x00FFFF00
+    dd 0x0FF0FF00
+    dd 0xFF00FF00
+    dd 0xFFFFFFF0
+    dd 0x0000FF00
+    dd 0x000FFFF0
+    dd 0x00000000
+glyph_35:
+    dd 0xFFFFFF00
+    dd 0xFF000000
+    dd 0xFFFFF000
+    dd 0x0000FF00
+    dd 0x0000FF00
+    dd 0xFF00FF00
+    dd 0x0FFFF000
+    dd 0x00000000
+glyph_36:
+    dd 0x00FFF000
+    dd 0x0FF00000
+    dd 0xFF000000
+    dd 0xFFFFF000
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0x0FFFF000
+    dd 0x00000000
+glyph_37:
+    dd 0xFFFFFF00
+    dd 0xFF00FF00
+    dd 0x0000FF00
+    dd 0x000FF000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00000000
+glyph_38:
+    dd 0x0FFFF000
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0x0FFFF000
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0x0FFFF000
+    dd 0x00000000
+glyph_39:
+    dd 0x0FFFF000
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0x0FFFFF00
+    dd 0x0000FF00
+    dd 0x000FF000
+    dd 0x0FFF0000
+    dd 0x00000000
+glyph_3A:
+    dd 0x00000000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00000000
+glyph_3B:
+    dd 0x00000000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x0FF00000
+glyph_3C:
+    dd 0x000FF000
+    dd 0x00FF0000
+    dd 0x0FF00000
+    dd 0xFF000000
+    dd 0x0FF00000
+    dd 0x00FF0000
+    dd 0x000FF000
+    dd 0x00000000
+glyph_3D:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0xFFFFFF00
+    dd 0x00000000
+    dd 0x00000000
+    dd 0xFFFFFF00
+    dd 0x00000000
+    dd 0x00000000
+glyph_3E:
+    dd 0x0FF00000
+    dd 0x00FF0000
+    dd 0x000FF000
+    dd 0x0000FF00
+    dd 0x000FF000
+    dd 0x00FF0000
+    dd 0x0FF00000
+    dd 0x00000000
+glyph_3F:
+    dd 0x0FFFF000
+    dd 0xFF00FF00
+    dd 0x0000FF00
+    dd 0x000FF000
+    dd 0x00FF0000
+    dd 0x00000000
+    dd 0x00FF0000
+    dd 0x00000000
+glyph_40:
+    dd 0x0FFFFF00
+    dd 0xFF000FF0
+    dd 0xFF0FFFF0
+    dd 0xFF0FFFF0
+    dd 0xFF0FFFF0
+    dd 0xFF000000
+    dd 0x0FFFF000
+    dd 0x00000000
+glyph_41:
+    dd 0x00FF0000
+    dd 0x0FFFF000
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFFFFFF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0x00000000
+glyph_42:
+    dd 0xFFFFFF00
+    dd 0x0FF00FF0
+    dd 0x0FF00FF0
+    dd 0x0FFFFF00
+    dd 0x0FF00FF0
+    dd 0x0FF00FF0
+    dd 0xFFFFFF00
+    dd 0x00000000
+glyph_43:
+    dd 0x00FFFF00
+    dd 0x0FF00FF0
+    dd 0xFF000000
+    dd 0xFF000000
+    dd 0xFF000000
+    dd 0x0FF00FF0
+    dd 0x00FFFF00
+    dd 0x00000000
+glyph_44:
+    dd 0xFFFFF000
+    dd 0x0FF0FF00
+    dd 0x0FF00FF0
+    dd 0x0FF00FF0
+    dd 0x0FF00FF0
+    dd 0x0FF0FF00
+    dd 0xFFFFF000
+    dd 0x00000000
+glyph_45:
+    dd 0xFFFFFFF0
+    dd 0x0FF000F0
+    dd 0x0FF0F000
+    dd 0x0FFFF000
+    dd 0x0FF0F000
+    dd 0x0FF000F0
+    dd 0xFFFFFFF0
+    dd 0x00000000
+glyph_46:
+    dd 0xFFFFFFF0
+    dd 0x0FF000F0
+    dd 0x0FF0F000
+    dd 0x0FFFF000
+    dd 0x0FF0F000
+    dd 0x0FF00000
+    dd 0xFFFF0000
+    dd 0x00000000
+glyph_47:
+    dd 0x00FFFF00
+    dd 0x0FF00FF0
+    dd 0xFF000000
+    dd 0xFF000000
+    dd 0xFF00FFF0
+    dd 0x0FF00FF0
+    dd 0x00FFFFF0
+    dd 0x00000000
+glyph_48:
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFFFFFF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0x00000000
+glyph_49:
+    dd 0x0FFFF000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x0FFFF000
+    dd 0x00000000
+glyph_4A:
+    dd 0x000FFFF0
+    dd 0x0000FF00
+    dd 0x0000FF00
+    dd 0x0000FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0x0FFFF000
+    dd 0x00000000
+glyph_4B:
+    dd 0xFFF00FF0
+    dd 0x0FF00FF0
+    dd 0x0FF0FF00
+    dd 0x0FFFF000
+    dd 0x0FF0FF00
+    dd 0x0FF00FF0
+    dd 0xFFF00FF0
+    dd 0x00000000
+glyph_4C:
+    dd 0xFFFF0000
+    dd 0x0FF00000
+    dd 0x0FF00000
+    dd 0x0FF00000
+    dd 0x0FF000F0
+    dd 0x0FF00FF0
+    dd 0xFFFFFFF0
+    dd 0x00000000
+glyph_4D:
+    dd 0xFF000FF0
+    dd 0xFFF0FFF0
+    dd 0xFFFFFFF0
+    dd 0xFFFFFFF0
+    dd 0xFF0F0FF0
+    dd 0xFF000FF0
+    dd 0xFF000FF0
+    dd 0x00000000
+glyph_4E:
+    dd 0xFF000FF0
+    dd 0xFFF00FF0
+    dd 0xFFFF0FF0
+    dd 0xFF0FFFF0
+    dd 0xFF00FFF0
+    dd 0xFF000FF0
+    dd 0xFF000FF0
+    dd 0x00000000
+glyph_4F:
+    dd 0x00FFF000
+    dd 0x0FF0FF00
+    dd 0xFF000FF0
+    dd 0xFF000FF0
+    dd 0xFF000FF0
+    dd 0x0FF0FF00
+    dd 0x00FFF000
+    dd 0x00000000
+glyph_50:
+    dd 0xFFFFFF00
+    dd 0x0FF00FF0
+    dd 0x0FF00FF0
+    dd 0x0FFFFF00
+    dd 0x0FF00000
+    dd 0x0FF00000
+    dd 0xFFFF0000
+    dd 0x00000000
+glyph_51:
+    dd 0x0FFFF000
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF0FFF00
+    dd 0x0FFFF000
+    dd 0x000FFF00
+    dd 0x00000000
+glyph_52:
+    dd 0xFFFFFF00
+    dd 0x0FF00FF0
+    dd 0x0FF00FF0
+    dd 0x0FFFFF00
+    dd 0x0FF0FF00
+    dd 0x0FF00FF0
+    dd 0xFFF00FF0
+    dd 0x00000000
+glyph_53:
+    dd 0x0FFFF000
+    dd 0xFF00FF00
+    dd 0xFFF00000
+    dd 0x0FFF0000
+    dd 0x000FFF00
+    dd 0xFF00FF00
+    dd 0x0FFFF000
+    dd 0x00000000
+glyph_54:
+    dd 0xFFFFFF00
+    dd 0xF0FF0F00
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x0FFFF000
+    dd 0x00000000
+glyph_55:
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFFFFFF00
+    dd 0x00000000
+glyph_56:
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0x0FFFF000
+    dd 0x00FF0000
+    dd 0x00000000
+glyph_57:
+    dd 0xFF000FF0
+    dd 0xFF000FF0
+    dd 0xFF000FF0
+    dd 0xFF0F0FF0
+    dd 0xFFFFFFF0
+    dd 0xFFF0FFF0
+    dd 0xFF000FF0
+    dd 0x00000000
+glyph_58:
+    dd 0xFF000FF0
+    dd 0xFF000FF0
+    dd 0x0FF0FF00
+    dd 0x00FFF000
+    dd 0x00FFF000
+    dd 0x0FF0FF00
+    dd 0xFF000FF0
+    dd 0x00000000
+glyph_59:
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0x0FFFF000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x0FFFF000
+    dd 0x00000000
+glyph_5A:
+    dd 0xFFFFFFF0
+    dd 0xFF000FF0
+    dd 0xF000FF00
+    dd 0x000FF000
+    dd 0x00FF00F0
+    dd 0x0FF00FF0
+    dd 0xFFFFFFF0
+    dd 0x00000000
+glyph_5B:
+    dd 0x0FFFF000
+    dd 0x0FF00000
+    dd 0x0FF00000
+    dd 0x0FF00000
+    dd 0x0FF00000
+    dd 0x0FF00000
+    dd 0x0FFFF000
+    dd 0x00000000
+glyph_5C:
+    dd 0xFF000000
+    dd 0x0FF00000
+    dd 0x00FF0000
+    dd 0x000FF000
+    dd 0x0000FF00
+    dd 0x00000FF0
+    dd 0x000000F0
+    dd 0x00000000
+glyph_5D:
+    dd 0x0FFFF000
+    dd 0x000FF000
+    dd 0x000FF000
+    dd 0x000FF000
+    dd 0x000FF000
+    dd 0x000FF000
+    dd 0x0FFFF000
+    dd 0x00000000
+glyph_5E:
+    dd 0x000F0000
+    dd 0x00FFF000
+    dd 0x0FF0FF00
+    dd 0xFF000FF0
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+glyph_5F:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0xFFFFFFFF
+glyph_60:
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x000FF000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+glyph_61:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x0FFFF000
+    dd 0x0000FF00
+    dd 0x0FFFFF00
+    dd 0xFF00FF00
+    dd 0x0FFF0FF0
+    dd 0x00000000
+glyph_62:
+    dd 0xFFF00000
+    dd 0x0FF00000
+    dd 0x0FF00000
+    dd 0x0FFFFF00
+    dd 0x0FF00FF0
+    dd 0x0FF00FF0
+    dd 0xFF0FFF00
+    dd 0x00000000
+glyph_63:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x0FFFF000
+    dd 0xFF00FF00
+    dd 0xFF000000
+    dd 0xFF00FF00
+    dd 0x0FFFF000
+    dd 0x00000000
+glyph_64:
+    dd 0x000FFF00
+    dd 0x0000FF00
+    dd 0x0000FF00
+    dd 0x0FFFFF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0x0FFF0FF0
+    dd 0x00000000
+glyph_65:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x0FFFF000
+    dd 0xFF00FF00
+    dd 0xFFFFFF00
+    dd 0xFF000000
+    dd 0x0FFFF000
+    dd 0x00000000
+glyph_66:
+    dd 0x00FFF000
+    dd 0x0FF0FF00
+    dd 0x0FF00000
+    dd 0xFFFF0000
+    dd 0x0FF00000
+    dd 0x0FF00000
+    dd 0xFFFF0000
+    dd 0x00000000
+glyph_67:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x0FFF0FF0
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0x0FFFFF00
+    dd 0x0000FF00
+    dd 0xFFFFF000
+glyph_68:
+    dd 0xFFF00000
+    dd 0x0FF00000
+    dd 0x0FF0FF00
+    dd 0x0FFF0FF0
+    dd 0x0FF00FF0
+    dd 0x0FF00FF0
+    dd 0xFFF00FF0
+    dd 0x00000000
+glyph_69:
+    dd 0x00FF0000
+    dd 0x00000000
+    dd 0x0FFF0000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x0FFFF000
+    dd 0x00000000
+glyph_6A:
+    dd 0x0000FF00
+    dd 0x00000000
+    dd 0x0000FF00
+    dd 0x0000FF00
+    dd 0x0000FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0x0FFFF000
+glyph_6B:
+    dd 0xFFF00000
+    dd 0x0FF00000
+    dd 0x0FF00FF0
+    dd 0x0FF0FF00
+    dd 0x0FFFF000
+    dd 0x0FF0FF00
+    dd 0xFFF00FF0
+    dd 0x00000000
+glyph_6C:
+    dd 0x0FFF0000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x0FFFF000
+    dd 0x00000000
+glyph_6D:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0xFF00FF00
+    dd 0xFFFFFFF0
+    dd 0xFFFFFFF0
+    dd 0xFF0F0FF0
+    dd 0xFF000FF0
+    dd 0x00000000
+glyph_6E:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0xFFFFF000
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0x00000000
+glyph_6F:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x0FFFF000
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0x0FFFF000
+    dd 0x00000000
+glyph_70:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0xFF0FFF00
+    dd 0x0FF00FF0
+    dd 0x0FF00FF0
+    dd 0x0FFFFF00
+    dd 0x0FF00000
+    dd 0xFFFF0000
+glyph_71:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x0FFF0FF0
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0x0FFFFF00
+    dd 0x0000FF00
+    dd 0x000FFFF0
+glyph_72:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0xFF0FFF00
+    dd 0x0FFF0FF0
+    dd 0x0FF00FF0
+    dd 0x0FF00000
+    dd 0xFFFF0000
+    dd 0x00000000
+glyph_73:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x0FFFFF00
+    dd 0xFF000000
+    dd 0x0FFFF000
+    dd 0x0000FF00
+    dd 0xFFFFF000
+    dd 0x00000000
+glyph_74:
+    dd 0x000F0000
+    dd 0x00FF0000
+    dd 0x0FFFFF00
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x00FF0F00
+    dd 0x000FF000
+    dd 0x00000000
+glyph_75:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0x0FFF0FF0
+    dd 0x00000000
+glyph_76:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0xFF00FF00
+    dd 0x0FFFF000
+    dd 0x00FF0000
+    dd 0x00000000
+glyph_77:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0xFF000FF0
+    dd 0xFF0F0FF0
+    dd 0xFFFFFFF0
+    dd 0xFFFFFFF0
+    dd 0x0FF0FF00
+    dd 0x00000000
+glyph_78:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0xFF000FF0
+    dd 0x0FF0FF00
+    dd 0x00FFF000
+    dd 0x0FF0FF00
+    dd 0xFF000FF0
+    dd 0x00000000
+glyph_79:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x0FF00FF0
+    dd 0x0FF00FF0
+    dd 0x0FF00FF0
+    dd 0x00FFFFF0
+    dd 0x00000FF0
+    dd 0x0FFFFF00
+glyph_7A:
+    dd 0x00000000
+    dd 0x00000000
+    dd 0xFFFFFF00
+    dd 0xF00FF000
+    dd 0x00FF0000
+    dd 0x0FF00F00
+    dd 0xFFFFFF00
+    dd 0x00000000
+glyph_7B:
+    dd 0x000FFF00
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0xFFF00000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x000FFF00
+    dd 0x00000000
+glyph_7C:
+    dd 0x000FF000
+    dd 0x000FF000
+    dd 0x000FF000
+    dd 0x00000000
+    dd 0x000FF000
+    dd 0x000FF000
+    dd 0x000FF000
+    dd 0x00000000
+glyph_7D:
+    dd 0xFFF00000
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0x000FFF00
+    dd 0x00FF0000
+    dd 0x00FF0000
+    dd 0xFFF00000
+    dd 0x00000000
+glyph_7E:
+    dd 0x0FFF0FF0
+    dd 0xFF0FFF00
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
